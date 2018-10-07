edcd78f76c288e72eeca6cf01eb02d59ae33915d
miritih <ericmwenda5@gmail.com>
2018-10-08 00:19:01 +0300

    Add filtering

--- server.rb
+++ server.rb
@@ -20,6 +20,10 @@ class Book
 
   index({ title: 'text' })
   index({ isbn:1 }, { unique: true, name: "isbn_index" })
+
+  scope :title, -> (title) { where(title: /^#{title}/) }
+  scope :isbn, -> (isbn) { where(isbn: isbn) }
+  scope :author, -> (author) { where(author: author) }
 end
 
 # Endpoints
@@ -33,7 +37,11 @@ namespace '/api/v1' do
   end
 
   get '/books' do
-    Book.all.to_json
+    books = Book.all
+     [:title, :isbn, :author].each do |filter|
+      books = books.send(filter, params[filter]) if params[filter]
+    end
+    books.to_json
   end
 
 end
